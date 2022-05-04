2eb6520560bccdc05c3931cf2384d1c753815ba0
bmalbright <brianalbright@utexas.edu>
2022-05-03 19:38:04 -0500

    created app. created components for newsfeed, exchange rate, and currency converter. Deleted unnecessary files. added some styling for testing purposes.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import CurrencyConverter from "./components/CurrencyConverter";
+// import ExchangeRate from "./components/ExchangeRate";
+import NewsFeed from "./components/NewsFeed";
+
+
+function App() {
+  return (
+    <div className="App">
+
+        <CurrencyConverter/>
+        <NewsFeed/>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CurrencyConverter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ExchangeRate from './ExchangeRate'
+
+export default function CurrencyConverter() {
+  return (
+    <div className='CurrencyConverter'>
+        CurrencyConverter
+        <ExchangeRate/>
+    </div>
+  )
+}
